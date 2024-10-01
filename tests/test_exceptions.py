68acb3488a2803e3f9529e5f504872afd0331cdd
Borja Esteban <boressan@outlook.com>
2024-10-01 14:22:30 +0200

    add docker hub validation

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -5,19 +5,6 @@ from cookiecutter.exceptions import FailedHookException
 from cookiecutter.main import cookiecutter
 
 CONFIGURATIONS_PATH = Path(__file__).parents[0] / "configurations"
-CONFIGURATIONS = set(["no-config"])
-
-
-@pytest.mark.parametrize("config_file", CONFIGURATIONS, indirect=True)
-def test_pre_gen_fails(cookiecutter_path, config_args):
-    with pytest.raises(FailedHookException) as excinfo:
-        cookiecutter(
-            template=str(cookiecutter_path),
-            no_input=True,
-            extra_context=config_args,
-            output_dir="project",
-        )
-    assert "Hook script failed" in str(excinfo.value)
 
 
 @pytest.mark.parametrize("config_file", ["bad-name"], indirect=True)
@@ -34,6 +21,20 @@ def test_bad_name(cookiecutter_path, config_args):
 
 @pytest.mark.parametrize("config_file", ["bad-submodule"], indirect=True)
 def test_bad_submodule(cookiecutter_path, config_args):
+    with pytest.raises(FailedHookException) as excinfo:
+        cookiecutter(
+            template=str(cookiecutter_path),
+            no_input=True,
+            extra_context=config_args,
+            output_dir="project",
+        )
+    assert "Hook script failed" in str(excinfo.value)
+
+
+@pytest.mark.parametrize(
+    "config_file", ["bad-baseimage", "bad-cpu_tag", "bad-gpu_tag"], indirect=True
+)
+def test_bad_docker_image(cookiecutter_path, config_args):
     with pytest.raises(FailedHookException) as excinfo:
         cookiecutter(
             template=str(cookiecutter_path),
