13d66713cea5ed9b86a5e398188b56adcdffb449
Borja Esteban <boressan@outlook.com>
2024-10-07 14:16:55 +0200

    replace git_base_url for https://github.com/ai4os-hub

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -95,7 +95,7 @@ try:
 
     # Initialise git repository and create test and dev branches
     git_repo_url = git_init(
-        base_url="{{ cookiecutter.git_base_url }}",
+        base_url="https://github.com/ai4os-hub",
         project_name="{{ cookiecutter.__repo_name }}",
     )
 
